1b866dabd25ca0318aec71d9bfdb5ef99c94ca06
Stelios Katsanevakis <nonofy@proton.me>
2023-09-28 23:48:54 +0200

    Make enemies a bit more interesting
    Now enemies are spawned from both sides and their speed is a bit slower

--- scenes/game/characters/enemy/enemy.gd
+++ scenes/game/characters/enemy/enemy.gd
@@ -4,37 +4,49 @@ class_name Enemy
 @export var bomb_scene : PackedScene
 signal died
 
-var max_speed = 400
+var max_speed = 250
 var hits = 0
-var aground = false
+var fly_direction = 1
+
+enum {FLY, HIT, FALL, AGROUND, DEAD}
+var state = FLY
 
 func _ready():
-	$BombTimer.start(0.5 + randf_range(-0.3, 1))
+	$BombTimer.start(randf_range(0.2, 1))
 
 func _process(delta):		
-	velocity = Vector2(1, 0).rotated(rotation) * max_speed
+	if state == HIT and rotation < PI/5:
+		rotation += fly_direction * 0.01
+		if abs(rotation) >= PI/5:
+			state = FALL
+	
+	velocity = Vector2(fly_direction, 0).rotated(rotation) * max_speed
 	if is_on_floor():
 		velocity = Vector2.ZERO
-		if not aground:
-			aground = true
+		if state != AGROUND:
+			state = AGROUND
 			$Animations/Grounding.play()
-			$DeathTimer.start(0.8)			
+			$DeathTimer.start(0.5)			
 		
 	move_and_slide()
 
+
 func _on_visible_on_screen_notifier_2d_screen_exited():
 	queue_free()
+
 
 func _on_area_2d_area_entered(area):
 	if hits == 0:
 		$BombTimer.stop()
 		died.emit()
-		rotation = PI/5 + randf_range(-0.2,0.2)		
+		state = HIT
 	else:
+		state = DEAD
 		$Animations/AirExplosion.play()
 		$PlanePivot/PlaneAnimation.hide()
-		$DeathTimer.start(0.3)
+		$DeathTimer.start(0.5)
 	hits += 1
+
 
 func _on_death_timer_timeout():
 	queue_free()
@@ -42,7 +54,10 @@ func _on_death_timer_timeout():
 
 func _on_bomb_timer_timeout():
 	var bomb = bomb_scene.instantiate()
-	print(position)
-	print(get_real_velocity())
 	bomb.init(position + Vector2(0, 20), get_real_velocity())
 	add_sibling(bomb)
+	
+func invert_direction():
+	fly_direction = -fly_direction
+	$PlanePivot/PlaneAnimation.scale *= Vector2(fly_direction, 1)
+	$Animations/Grounding.scale *= Vector2(fly_direction, 1)
