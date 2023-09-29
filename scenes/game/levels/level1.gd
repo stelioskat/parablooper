1bed9c6352293f64f8d1a96ebda1b694e6dffe0a
Stelios Katsanevakis <nonofy@proton.me>
2023-09-29 21:50:58 +0200

    Increase resolution to 1920x1080
    Currently many of the mechanics of the game depend on the screen resolution. They break as soon as changed. This is a huge design flaw and needs to be adressed

--- scenes/game/levels/level1.gd
+++ scenes/game/levels/level1.gd
@@ -24,36 +24,38 @@ func _on_canon_fire(bullet_spawn_location):
 
 
 func _on_enemy_timer_timeout():
-	$EnemySpawnTimer.wait_time = randf_range(0.8, 1.2)
+	$EnemySpawnTimer.wait_time = randf_range(1, 1.5)
 	var enemy = enemy_scene.instantiate()
 	var side = randf_range(-1,1)
 	if side < 0:
-		enemy.position = Vector2(-65, randi_range(100,200))
+		enemy.position = Vector2(-65, randi_range(50,600))
 	else:
 		var width = get_viewport().get_visible_rect().size.x
-		enemy.position = Vector2(width+65, randi_range(100,200))
+		enemy.position = Vector2(width+65, randi_range(50,600))
 		enemy.invert_direction()
 	
 	enemy.died.connect(_on_enemy_died)
 	add_child(enemy)
 
+func score(points):
+	# for now we always give 1 point for each kilö
+	scored.emit(points)
+
 
 func _on_enemy_died():
-	# for now we always give 1 point for each kilö
-	scored.emit(1)
-	pass
-	
+	score(10)
+
+
 func apply_damage(damage_origin : Vector2):
 	# later we could use player/enemy stats or a damage/attack object :thinking:
 	var max_damage = 10
-	var range = 60
+	var range = 80
 	
 	var nodes = get_tree().get_nodes_in_group("has_health")
 	for node in nodes:
 		var dist = node.position.distance_to(damage_origin)
 		var target_damage = 0
 		if dist <= range:
-			target_damage = (1 - range/3**dist) * max_damage
-		print("dist: %s" % dist)
-		print("dmg: %s" % target_damage)
-		node.take_damage(target_damage)
+			target_damage = roundi((1-dist**2/range**2) * max_damage)
+		if target_damage > 0:
+			node.get_node("Health").take_damage(target_damage)
