9664bc512a0905e45770c664c029a5f3e51e0428
Stelios Katsanevakis <nonofy@proton.me>
2023-09-29 21:49:23 +0200

    Add fancy sound effects

--- scenes/game/characters/bomb/bomb.gd
+++ scenes/game/characters/bomb/bomb.gd
@@ -1,26 +1,34 @@
 extends RigidBody2D
+class_name Bomb
 
 signal exploded
+signal died # indicates when user stopped it
 var is_exploded = false
 
 
 func _ready():
 	set_freeze_mode(FREEZE_MODE_KINEMATIC)
+	$FallingFX.play()
 #	init(Vector2.ZERO, Vector2(400,0))
 
 
 func init(start_position : Vector2, start_velocity : Vector2):
 	position = start_position
+	if(start_velocity.x > 0):
+		$BombSprite.flip_h = true
 	set_axis_velocity(start_velocity)
-
 
 func _on_body_entered(body):
 	explode()
 
 func _on_area_2d_area_entered(area):
+	if area.get_collision_mask_value(3):
+		get_tree().call_group("level", "score", 15)
 	explode()
 
 func explode():
+	$FallingFX.stop()
+	$ExplodeFX.play()
 	if is_exploded:
 		return
 		
@@ -39,4 +47,5 @@ func _on_unspawn_timer_timeout():
 	queue_free()
 
 
-
+func _on_tree_entered():
+	pass
